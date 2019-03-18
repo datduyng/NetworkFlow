31379c93db1415c956e1455bc60c3d5153a40abe
Dat Nguyen <datnguyen5653@gmail.com>
2019-03-17 21:49:50 -0500

    Added unstable version for Simulator. Moving car. no turns. Also Code clean up

--- Car.js
+++ Car.js
@@ -1,34 +1,151 @@
-function Car(x, y, degree){
+function Car(x, y, xIndex, yIndex,  degree){
 	this.texture = PIXI.Texture.fromImage(spritePath['car']);
 	PIXI.Sprite.call(this, this.texture, tileSize/2,tileSize); 
 	this.position.x = x;// set initial position
 	this.position.y = y;
+	this.xIndex = xIndex; 
+	this.yIndex = yIndex; 
+	this.id; //TODO: config id attr 
 
-	this.dirMap = {
+	dirMap = {
 		0:'>',
 		90:'v',
 		180:'<',
 		270:'^'
 	};
-	this.direction = this.dirMap[degree];
+
+	dir2degree = {
+		'>' : 0,
+		'v' : 90,
+		'<' : 180,
+		'^' : 270
+	};
+
+	if(typeof degree == 'number'){
+		this.direction = dirMap[degree];
+		this.rotation = degree * Math.PI / 180;
+	}
+	else{
+		this.direction = degree;
+		this.rotation = dir2degree[degree] * Math.PI / 180;
+	}
 	this.anchor.set(0.5, 0); //set pivot point. at top middle. 
-	this.rotation = degree * Math.PI / 180;
-	renderer.render(stage);
+	
+
+	//Car Simulation Attribute
+	this.initalSpeed = 60; //mph
+	this.speed = 120; //mph
+	this.tick = 0;//timer keep to track in between 2 tile. Instead of keeping distance traveled
+	this.state = 'idle';//'idle', 'regular', 'stop', 
+
+	this.gapTime = {
+		'idle' : 1, // tick
+		// 'regular' : distBetween2Tile / this.speed * 3600 / (TIME_UNIT/1000), // tick
+		// 'acel' : distBetween2Tile / this.initalSpeed * 3600 / (TIME_UNIT/1000) // tick
+		'regular' : 60,
+		'acel' : 120
+	};
 }
 Car.prototype = Object.create(PIXI.Sprite.prototype);
 
 // function of Car class
-Car.prototype.setX = function(x){
+Car.prototype.setXY = function(x, y){
 	this.position.x = x;
+	this.position.y = y; 
 }
-Car.prototype.setY = function(y){
-	this.position.y = y;
+
+Car.prototype.setXYIndex = function(xIndex, yIndex){
+	this.xIndex = xIndex; 
+	this.yIndex = yIndex;
 }
+
+
+Car.prototype.updateCarTilePosition = function(){
+	
+	if(this.direction == '>'){
+		this.setXYIndex(this.xIndex+1, this.yIndex); 
+	}else if (this.direction == '<'){
+		this.setXYIndex(this.xIndex-1, this.yIndex);
+	}else if(this.direction == '^'){
+		this.setXYIndex(this.xIndex, this.yIndex-1);
+	}else if(this.direction == 'v'){
+		this.setXYIndex(this.xIndex, this.yIndex+1);
+	}
+}
+
+Car.prototype.moveDistance = function(distance){
+	//scale up to our actual software pixel
+	if(this.direction == '>'){
+		this.setXY(this.position.x+distance, this.position.y); 
+	}else if (this.direction == '<'){
+		this.setXY(this.position.x-distance, this.position.y);
+	}else if(this.direction == '^'){
+		this.setXY(this.position.x, this.position.y-distance);
+	}else if(this.direction == 'v'){
+		this.setXY(this.position.x, this.position.y+distance);
+	}
+}
+
+Car.prototype.adjustCarVisualization = function(totalDist){
+	//TODO: make the adjustment become smoother. 
+	this.moveDistance(totalDist);
+}
+
+
+Car.prototype.move = function(nextState){
+	if(nextState == 'stop'){//high priority
+		this.state = stop;
+	}
+	if(this.state == 'idle'){
+		if(this.tick / this.gapTime[this.state] >= 1.0){
+			this.state = nextState;
+		}
+
+	}else if(this.state == 'acel'){
+		var distanceTraveled = 1 / this.gapTime[this.state];
+		this.moveDistance(distanceTraveled*tileSize );
+		if((this.tick / this.gapTime[this.state]) >= 1.0){
+			this.updateCarTilePosition();
+			if(this.direction == '>' || this.direction == '<')
+				//-25 for offset. anchor attop mid
+				this.adjustCarVisualization(Math.abs(Math.max(this.xIndex*tileSize, this.position.x-25)) - Math.min(this.xIndex*tileSize, this.position.x-25));
+			else if(this.direction == '^' || this.direction == 'v')
+				this.adjustCarVisualization(Math.abs(Math.max(this.yIndex*tileSize, this.position.y-25)) - Math.min(this.yIndex*tileSize, this.position.y-25));
+			
+			this.state = nextState;
+			this.tick = 0;//restart timer
+		}
+
+	}else if(this.state == 'regular'){
+		var distanceTraveled = 1 / this.gapTime[this.state];
+		this.moveDistance(distanceTraveled*tileSize);
+		if((this.tick / this.gapTime[this.state]) >= 1.0){
+			this.updateCarTilePosition()
+			if(this.direction == '>' || this.direction == '<')
+				this.adjustCarVisualization(Math.abs(Math.max(this.xIndex*tileSize, this.position.x-25)) - Math.min(this.xIndex*tileSize, this.position.x-25));
+			else if(this.direction == '^' || this.direction == 'v')
+				this.adjustCarVisualization(Math.abs(Math.max(this.yIndex*tileSize, this.position.y-25)) - Math.min(this.yIndex*tileSize, this.position.y-25));
+			
+			this.state = nextState;
+			this.tick = 0;//restart tick timer
+		}
+	}else if(this.state == 'turn'){
+		
+	}else if(this.state == 'stop'){
+
+	}
+	this.tick+=1;
+}
+
 
 Car.prototype.toString = function(){
 	return JSON.stringify({
-		'x' : this.position.x,
-		'y' : this.position.y,
+		'xIndex' : this.xIndex,
+		'yIndex' : this.yIndex,
+		'pixi.position.x' : this.position.x, 
+		'pixi.position.y' : this.position.y,
 		'direction' : this.direction,
+		'tick' : this.tick, 
+		'state' : this.state
 	});
 }
--- Tile.js
+++ Tile.js
@@ -1,84 +1,48 @@
-function Tile(type, tileSize){
-	this.type = type; 
-
-	this.texture;
-	this.swapTexture(type);// configure the right
+function Tile(classType, tileSize){
+    // this.swapTexture(generalType);// configure the right
     // texture for tiel
-	//inheritance with Sprite class
-	PIXI.Sprite.call(this, this.texture, tileSize, tileSize);
-	this.position.x = 0;// set initial position
-	this.position.y = 0;
-    this.indexX = -1;
-    this.indexY = -1;
-}
-Tile.prototype = Object.create(PIXI.Sprite.prototype);
+    //inheritance with Sprite class
 
+    this.createTileClassType(classType);
+
+    this.tileClass.position.x = 0;// set initial position
+    this.tileClass.position.y = 0;
+    this.xIndex = -1;
+    this.yIndex = -1;
+}
 
 
 Tile.prototype.setIndexXY = function(xIndex, yIndex){
-    this.indexX = xIndex;
-    this.indexY = yIndex;
+    this.xIndex = xIndex;
+    this.yIndex = yIndex;
 }
 
 // function of Tile class
 Tile.prototype.setXY = function(x, y){
-	this.position.x = x;
-    this.position.y = y; 
+    this.tileClass.position.x = x;
+    this.tileClass.position.y = y; 
 }
 
-Tile.prototype.swapTexture = function(type){
-    if(type != 'car'){//change tile to new type// except for car
-        this.type = type;
-    }
 
-	if(type == 'grass'){
-		this.texture = PIXI.Texture.fromImage(spritePath[type]);
-	}else if(type == 'ground'){
-		this.texture = PIXI.Texture.fromImage(spritePath[type]);
-	}else if(type == 'road-horizontal'){
-		this.texture = PIXI.Texture.fromImage(spritePath[type]);
-	}else if(type == 'road-verticle'){
-		this.texture = PIXI.Texture.fromImage(spritePath[type]);
-	}else if(type == 'stop-sign'){
-		this.texture = PIXI.Texture.fromImage(spritePath[type]);
-	}else if(type == 'traffic-light'){
-        this.texture = PIXI.Texture.fromImage(spritePath[type])
-    }else if(type == 'construction-man'){
-		this.texture = PIXI.Texture.fromImage(spritePath[type]);
-	}else if(type == 'construction-barrier'){
-		this.texture = PIXI.Texture.fromImage(spritePath[type]);
-	}else if(type == 'car'){
-        var x = this.position.x + tileSize/2;
-        var y = this.position.y + tileSize/2;
-
-        var car = new Car(x, y, degree);
-        stage.addChild(car);
-        renderer.render(stage);
-
-        carList.push(car);
-    }else{
-        console.log("Tile Nothing");
-    }
-    
-}
 
 Tile.prototype.setInteractive = function(){
-	this.interactive = true;
-    this.on('mouseover', (event) => {
-        this.tint = 0xB27D7D;
+    this.tileClass.interactive = true;
+    this.tileClass.on('mouseover', (event) => {
+        this.tileClass.tint = 0xB27D7D;
         if(down){// if hover and mouse down
             //build here
-            this.swapTexture(currentTileType)
+            this.createTileClassType(currentTileType)
         }
         renderer.render(stage);
     }).on('mouseout', (event) => {
-        	this.tint = 0xFFFFFF;
-            renderer.render(stage);
+        this.tileClass.tint = 0xFFFFFF;
+        renderer.render(stage);
     }).on('mousedown', (event) => {
         down = true;
         hold = true;
-       	this.swapTexture(currentTileType);
-        renderer.render(stage);
+        this.createTileClassType(currentTileType);
+        if(currentTileType == 'car') this.createObjectOnTop(currentTileType);
+        // renderer.render(stage);
     }).on('mouseup', (event) => {
         down = false;
         hold = false;
@@ -90,12 +54,93 @@ Tile.prototype.getContent = function(){
 }
 
 
-Tile.prototype.toString = function(){
-    if(this.type == 'road-horizontal' || this.type == 'road-verticle'){
-         return 'road';
-    }else if(this.type == 'construction-man' || this.type == 'construction-barrier' || 
-       this.type == 'grass'){
-        return 'ground';
+Tile.prototype.toString = function(softwaretype){
+    if(softwaretype == 'javafx'){
+        if(this.tileClass.tileClass.classType == 'road-horizontal' || this.tileClass.classType == 'road-verticle'){
+             return 'road';
+        }else if(this.tileClass.classType == 'construction-man' || this.tileClass.classType == 'construction-barrier' || 
+           this.tileClass.classType == 'grass'){
+            return 'ground';
+        }
+        return this.generalType;
     }
-    return this.type;
+    return this.tileClass.classType;
+
+}
+
+/**
+ * Added info to @params tile by references
+ */
+Tile.prototype.getOldTileInfo = function(tile){
+    tile.position.x = this.tileClass.position.x;
+    tile.position.y = this.tileClass.position.y; 
+}
+
+Tile.prototype.createTileClassType = function(classType){
+    if(classType == "grass" || classType == "ground" || classType=="construction-man"||
+       classType == "construction-barrier"){
+        var newTile = new Ground(classType);
+        
+
+        //after remove then transfre info
+        if (this.tileClass != null) {
+            this.getOldTileInfo(newTile);
+            stage.removeChild(this.tileClass);
+        }
+        this.tileClass = newTile;
+        this.setInteractive();
+        stage.addChild(this.tileClass);
+        this.generalType = "ground";
+    }else if(classType == "traffic-light"){
+        var newTile = new TrafficLight(classType);
+        //after remove then transfre info
+        if (this.tileClass != null) {
+            this.getOldTileInfo(newTile);
+            stage.removeChild(this.tileClass);
+        }
+        this.tileClass = newTile;
+        this.setInteractive();
+        stage.addChild(this.tileClass);
+        this.generalType = "traffic-light";
+    }else if(classType == "stop-sign"){
+        var newTile = new StopSign(classType);
+        //after remove then transfre info
+        if (this.tileClass != null) {
+            this.getOldTileInfo(newTile);
+            stage.removeChild(this.tileClass);
+        }
+        this.tileClass = newTile;
+        this.setInteractive();
+        stage.addChild(this.tileClass);
+        this.generalType = "stop-sign";
+    }else if(classType == "road-horizontal" || 
+             classType == "road-verticle"){
+        var newTile = new Road(classType);
+        //after remove then transfre info
+        if (this.tileClass != null) {
+            this.getOldTileInfo(newTile);
+            stage.removeChild(this.tileClass);
+        }
+        this.tileClass = newTile;
+        stage.addChild(this.tileClass);
+        this.setInteractive();
+        this.generalType = "road";
+    }else{
+        console.log("Invalid classType", classType);
+    }
+    
+}
+
+Tile.prototype.createObjectOnTop = function(classType){
+    if(classType == "car"){
+            console.log("classtype car");
+            var x = this.tileClass.position.x + tileSize/2;
+            var y = this.tileClass.position.y + tileSize/2;
+
+            var car = new Car(x, y, this.xIndex, this.yIndex, degree);
+            stage.addChild(car);
+            renderer.render(stage);
+
+            carList.push(car);
+    }
 }
